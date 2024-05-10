3f6c4b656450e84ed94366dcd512158d64c78503
Biraj Thapa <birajthapa983@gmail.com>
2024-05-10 15:09:56 +0545

    sidebar added

--- clone-spotify/src/App.jsx
+++ clone-spotify/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar/>
+
+
+      </div>
+    </div>
   )
 }
 
