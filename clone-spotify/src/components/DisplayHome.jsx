4c375aa4e95ef7ecd4c89abd49c2e1e9352c3b0e
Biraj Thapa <birajthapa983@gmail.com>
2024-05-10 22:02:47 +0545

    home ui added

--- clone-spotify/src/components/DisplayHome.jsx
+++ clone-spotify/src/components/DisplayHome.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import NavBar from './NavBar'
-
+import React from "react";
+import NavBar from "./NavBar";
+import { albumsData, songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
     <>
-    <NavBar/>
+      <NavBar />
+      <div className="mb-4 ">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4 ">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+            {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} image={item.image} />))}
+       
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
