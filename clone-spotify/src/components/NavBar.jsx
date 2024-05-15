890628171b42224f9dc3ffd35f2261d16545ff52
Biraj Thapa <birajthapa983@gmail.com>
2024-05-15 23:50:46 +0545

    created navigate button and added some colors in albums

--- clone-spotify/src/components/NavBar.jsx
+++ clone-spotify/src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+  const navigate=useNavigate()
   return (
     <>
     <div className='w-full flex justify-between items-center font-semibold'>
         <div className='flex items  gap-2'>
-            <img  className='w-8 bg-black p-2 rounded-2xl cursor-pointer'  src={assets.arrow_left} alt="" />
-            <img  className='w-8 bg-black p-2 rounded-2xl cursor-pointer'  src={assets.arrow_right} alt="" />
+            <img  onClick={()=>{navigate(-1)}} className='w-8 bg-black p-2 rounded-2xl cursor-pointer'  src={assets.arrow_left} alt="" />
+            <img onClick={()=>{navigate(1)}} className='w-8 bg-black p-2 rounded-2xl cursor-pointer'  src={assets.arrow_right} alt="" />
 
         </div>
         <div className='flex items-center gap-4'>
