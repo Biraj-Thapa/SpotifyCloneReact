9e9dee526742317b49f3689155206186250c12e0
Biraj Thapa <birajthapa983@gmail.com>
2024-05-17 15:51:12 +0545

    songs forward bacaward added

--- clone-spotify/src/components/Player.jsx
+++ clone-spotify/src/components/Player.jsx
@@ -4,7 +4,7 @@ import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
 
-  const {track,seekBar,seekBg,playStatus,play,pause,time}=useContext(PlayerContext);
+  const {track,seekBar,seekBg,playStatus,play,pause,time,previous,next}=useContext(PlayerContext);
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
@@ -24,11 +24,11 @@ const Player = () => {
             src={assets.shuffle_icon}
             alt=""
           />
-          <img className="w-4 cursor-pointer " src={assets.prev_icon} alt="" />
+          <img onClick={previous} className="w-4 cursor-pointer " src={assets.prev_icon} alt="" />
           {playStatus?<img onClick={pause} className="w-4 cursor-pointer " src={assets.pause_icon} alt="" />: <img onClick={play} className="w-4 cursor-pointer " src={assets.play_icon} alt="" />}
          
           
-          <img className="w-4 cursor-pointer " src={assets.next_icon} alt="" />
+          <img onClick={next} className="w-4 cursor-pointer " src={assets.next_icon} alt="" />
           <img className="w-4 cursor-pointer " src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
--- clone-spotify/src/context/PlayerContext.jsx
+++ clone-spotify/src/context/PlayerContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
+
 export const PlayerContext = createContext();
 
 const PlayerContextProvider = (props) => {
@@ -28,24 +29,47 @@ const PlayerContextProvider = (props) => {
     audioRef.current.pause();
     setPlayStatus(false);
   };
-  useEffect(()=>{
-    setTimeout(()=>{
-      audioRef.current.ontimeupdate = ()=>{
+
+  const playWithId= async (id)=>{
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    setPlayStatus(true)
+
+  };
+
+  const previous=async()=>{
+    if(track.id>0){
+      await setTrack(songsData[track.id-1]);
+      await audioRef.current.play();
+      setPlayStatus(true);
+    }
+
+  }
+  const next=async()=>{
+    if(track.id<songsData.length-1){
+      await setTrack(songsData[track.id-1]);
+      await audioRef.current.play();
+      setPlayStatus(true);
+    }
+
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width =(Math.floor(audioRef.current.currentTime/audioRef.current.duration *100))+"%"
         setTime({
           currentTime: {
-            second: Math.floor(audioRef.current.currentTime%60),
-            minute: Math.floor(audioRef.current.currentTime/60)
+            second: Math.floor(audioRef.current.currentTime % 60),
+            minute: Math.floor(audioRef.current.currentTime / 60),
           },
           totalTime: {
-            second: Math.floor(audioRef.current.duration%60),
-            minute: Math.floor(audioRef.current.duration/60)
-          }})
-      }
-
-    },1000)
-
-
-  },[audioRef])
+            second: Math.floor(audioRef.current.duration % 60),
+            minute: Math.floor(audioRef.current.duration / 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
 
   const ContextValue = {
     audioRef,
@@ -59,6 +83,9 @@ const PlayerContextProvider = (props) => {
     setTime,
     play,
     pause,
+    playWithId,
+    previous,
+    next
   };
   return (
     <PlayerContext.Provider value={ContextValue}>
